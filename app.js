87dc188a6813f21ef320290aa60d07a8754c33e7
suljiciman <suljic.iman@gmail.com>
2023-01-14 14:19:46 +0100

    bulbs now change colour

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 document.getElementById('stop').onclick = illuminateRed;
+document.getElementById('wait').onclick = illuminateYellow;
+document.getElementById('go').onclick = illuminateGreen;
 
 function illuminateRed() {
+  clearLights();
+  document.getElementById('stop').style.backgroundColor = "red";
+}
+
+function illuminateYellow() {
+  clearLights();
+  document.getElementById('wait').style.backgroundColor = "orange";
+}
+
+function illuminateGreen() {
     clearLights();
-    document.getElementById('stop').style.backgroundColor = "red";
+    document.getElementById('go').style.backgroundColor = "green";
   }
+
+
+function clearLights() {
+  document.getElementById('stop').style.backgroundColor = "black";
+  document.getElementById('wait').style.backgroundColor = "black";
+  document.getElementById('go').style.backgroundColor = "black";
+}
+
+
